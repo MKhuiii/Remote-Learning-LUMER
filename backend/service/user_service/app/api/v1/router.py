2f7e5416a34d9a500efb765c88cee0039ccc8434
TMT3011 <tranminhtu1130@gmail.com>
2026-06-29 11:39:09 +0700

    update router profile

--- backend/service/user_service/app/api/v1/router.py
+++ backend/service/user_service/app/api/v1/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from app.api.v1.routers.auth import router as auth_router
 from app.api.v1.routers.user import router as user_router
+from app.api.v1.routers.profile import router as profile_router
 
 router = APIRouter()
 router.include_router(auth_router)
 router.include_router(user_router)
+router.include_router(profile_router)
