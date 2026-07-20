c5a48c428993f5a493e4b88b0233fa36f1aa0eac
MKhuii <lyminhkhoi15122005@gmail.com>
2026-07-20 17:18:26 +0700

    Complete course-content page

---
+++ frontend/src/types/subject.ts
@@ -0,0 +1,8 @@
+export interface GeneralInfoInstructorSubject {
+    subject_id: string;
+    title: string;
+    description: string;
+    status_id: string;
+    total_modules: number;
+    total_lessons: number;
+}
