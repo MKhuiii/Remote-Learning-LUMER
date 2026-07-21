994dc1674e42b4661e50c1143ec04c19fdc48a4c
TMT3011 <tranminhtu1130@gmail.com>
2026-07-21 12:33:31 +0700

    remove conflict

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -28,9 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <GoogleProvider>
-          {children}
-        </GoogleProvider>
+        <GoogleProvider>{children}</GoogleProvider>
       </body>
     </html>
   );
